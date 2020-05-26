b877755042949b31888ce2158ae756b50a678fda
Junior Oliveira <arojunior@gmail.com>
2020-05-26 21:41:34 +0200

    remove flow and add airbnb config

--- index.js
+++ index.js
@@ -1,49 +1,52 @@
-
 module.exports = {
-  "extends": [
-    "plugin:flowtype/recommended",
-    "plugin:react/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "prettier",
-    "prettier/flowtype",
-    "prettier/react"
+  extends: [
+    'airbnb',
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'prettier',
+    'prettier/react',
+    'plugin:jsx-a11y/recommended',
   ],
-  "plugins": [
-    "flowtype",
-    "react",
-    "prettier"
-  ],
-  "globals": {
-    "document": false
+  plugins: ['react', 'prettier', 'jsx-a11y'],
+  globals: {
+    window: true,
+    document: true,
+    WebSocket: true,
+    shallow: true,
+    render: true,
+    mount: true,
   },
-  "parserOptions": {
-    "ecmaVersion": 2017,
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "jsx": true,
-      "experimentalObjectRestSpread": true,
-      "experimentalDecorators": true
-    }
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      experimentalObjectRestSpread: true,
+      experimentalDecorators: true,
+    },
   },
-  "env": {
-    "jest": true,
-    "node": true
+  env: {
+    jest: true,
+    node: true,
   },
-  "rules": {
-    "no-unused-vars": 1,
-    "no-duplicate-imports": 0,
-    "import/no-duplicates": 2,
-    "import/named": 0,
-    "import/no-unresolved": 0,
-    "react/prop-types": [1, { "skipUndeclared": true }],
-    "react/display-name": 0,
-    "prettier/prettier": ["error", {
-      "tabWidth": 2,
-      "printWidth": 80,
-      "singleQuote": true,
-      "jsxBracketSameLine": true,
-      "trailingComma": "es5"
-    }]
-  }
-};
+  rules: {
+    'no-unused-vars': 1,
+    'no-duplicate-imports': 0,
+    'import/no-duplicates': 2,
+    'import/named': 0,
+    'import/no-unresolved': 0,
+    'react/prop-types': [1, { skipUndeclared: true }],
+    'react/display-name': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        printWidth: 80,
+        singleQuote: true,
+        jsxBracketSameLine: true,
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
